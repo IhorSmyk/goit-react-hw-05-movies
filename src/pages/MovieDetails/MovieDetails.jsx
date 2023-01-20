d26438a5df2994b05c5583ffc20de7fad0fcdb20
Ihor Smyk <ismyk256@gmail.com>
2023-01-20 16:32:24 +0200

    upd

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect, Suspense, lazy } from 'react';
 import { useParams, Link, Route, Routes, useLocation } from 'react-router-dom';
 import { getMovieDetails } from 'services/API';
 import { Notify } from 'notiflix';
+import { BsFillArrowLeftSquareFill } from 'react-icons/bs';
 import s from './MovieDetails.module.css';
 
 const Cast = lazy(() => import('pages/Cast/Cast'));
@@ -34,71 +35,68 @@ function MovieDetails() {
 
   useEffect(() => {
     if (error === null) return;
-
     Notify.failure(`some error occured ${error}`);
   }, [error]);
 
   return (
     <div className={s.wrapper}>
-      <Link to={location?.state?.from ?? '/'}>go back</Link>
+      <Link className={s.goBack} to={location?.state?.from ?? '/'}>
+        <BsFillArrowLeftSquareFill size="30px" />
+      </Link>
       {isLoading && <Loader />}
-      <div>
-        {movieInfo !== null && (
-          <div>
-            <div>
-              <img
-                src={
-                  movieInfo?.poster_path
-                    ? `https://image.tmdb.org/t/p/w500${movieInfo?.poster_path}`
-                    : 'https://cdn4.iconfinder.com/data/icons/ui-beast-4/32/Ui-12-512.png'
-                }
-                alt={movieInfo.title}
-                className={s.img}
-              />
-            </div>
-            <div className={s.info}>
-              <h1>
-                {movieInfo.title} ({movieInfo?.release_date.slice(0, 4)})
-              </h1>
-              <p>User Score: {movieInfo?.vote_average}</p>
-              <h2>Overview</h2>
-              <p>{movieInfo.overview}</p>
-              <h2>Genres</h2>
-              <ul className={s.genre}>
-                {movieInfo?.genres?.map(({ id, name }) => {
-                  return (
-                    <li key={id} className={s.genreItem}>
-                      {name}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+      {movieInfo !== null && (
+        <div className={s.box}>
+          <img
+            className={s.img}
+            src={
+              movieInfo?.poster_path
+                ? `https://image.tmdb.org/t/p/w500${movieInfo?.poster_path}`
+                : 'https://cdn4.iconfinder.com/data/icons/ui-beast-4/32/Ui-12-512.png'
+            }
+            alt={movieInfo.title}
+          />
+          <div className={s.info}>
+            <h1 className={s.title}>
+              {movieInfo.title} ({movieInfo?.release_date.slice(0, 4)})
+            </h1>
+            <p>User Score: {movieInfo?.vote_average}</p>
+            <h2>Overview</h2>
+            <p className={s.overview}>{movieInfo.overview}</p>
+            <h2>Genres</h2>
+            <ul className={s.genre}>
+              {movieInfo?.genres?.map(({ id, name }) => {
+                return (
+                  <li key={id} className={s.genreItem}>
+                    {name}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
-        )}
-        <div className={s.moreInfo}>
-          <h2>More info</h2>
-          <Link
-            state={{ from: location?.state?.from ?? '/' }}
-            className={s.moreInfoLink}
-            to="reviews"
-          >
-            Reviews
-          </Link>
-          <Link
-            state={{ from: location?.state?.from ?? '/' }}
-            className={s.moreInfoLink}
-            to="cast"
-          >
-            Cast
-          </Link>
-          <Suspense fallback={<Loader/>}>
-            <Routes>
-              <Route path="cast" element={<Cast />} />
-              <Route path="reviews" element={<Reviews />} />
-            </Routes>
-          </Suspense>
         </div>
+      )}
+      <div className={s.moreInfo}>
+        {/* <h3 className={ s.moreInfoTitle}>More info</h3> */}
+        <Link
+          state={{ from: location?.state?.from ?? '/' }}
+          className={s.moreInfoLink}
+          to="reviews"
+        >
+          Reviews
+        </Link>
+        <Link
+          state={{ from: location?.state?.from ?? '/' }}
+          className={s.moreInfoLink}
+          to="cast"
+        >
+          Cast
+        </Link>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Routes>
+        </Suspense>
       </div>
     </div>
   );
